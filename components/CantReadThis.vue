7a7bcaf3ba059bb30daaa7f3c7dd469f0b78f762
nort3x <nort3x911@protonmail.com>
2023-11-18 14:42:18 +0330

    (init)

---
+++ components/CantReadThis.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col w-full gap-[0.5rem] items-center justify-center">
+    <div class="h-[50vh] w-[50vh] bg-white"></div>
+    <p class="text-[30px] font-bold">Scan if you can't read
+      <span class="text-[16px] p-3 bg-black border rounded">
+      this
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ slides.md
@@ -0,0 +1,15 @@
+---
+src: ./pages/0.splash.md
+---
+
+---
+src: ./pages/1.who_am_i_and_what_is_this_about.md
+---
+
+---
+src: ./pages/2.table_of_contents.md
+---
+
+---
+src: ./pages/6.outro.md
+---
